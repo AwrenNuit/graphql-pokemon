071a25ec4db00de0100579226eda45fc1f4089a7
AwrenNuit <a.rennaker@yahoo.com>
2020-04-20 10:57:13 -0500

    fixed outdated query structure, added basic styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import { useQuery } from '@apollo/react-hooks';
-import query from 'graphql-tag';
+import gql from 'graphql-tag';
 
-const GET_POKEMON_INFO = query`
+const GET_POKEMON_INFO = gql`
   {
-    pokemon(first: 150) {
+    pokemon(name: "Pikachu") {
       id
       number
       name,
@@ -17,49 +17,44 @@ const GET_POKEMON_INFO = query`
         image
       }
     }
-  }`
+  }`;
 
 export default function App() {
 
   const { data, loading, error } = useQuery(GET_POKEMON_INFO);
 
   if (loading) return <p>Loading...</p>;
-  if (error) {
-    console.log('query:', query);
-    console.log(error);
-    return <p>Error</p>;
-  }
+  if (error) return <p>Error</p>;
+
+  const p = data.pokemon;
 
   return (
     <>
       <h1>Pokémon</h1>
-      {JSON.stringify(data)}
       <p>
         Pokémon have been around for as long as humans can remember. While new species
         are being discovered each year, this is the original group of Pokémon. There 
-        were 150 in total back then. Now the number of known species has grown into the
-        800s. As humans continue to explore the world, there's no telling how many more
-        Pokémon will be discovered.
+        were 150 in total back then. Of those 150 Pikachu was the most famous. Now the 
+        number of known species has grown into the 800s. As humans continue to explore 
+        the world, there's no telling how many more Pokémon will be discovered.
       </p>
       <div className="container">
-        {data && data.pokemon && data.pokemon.map((p, i) => (
-            <div key={i} className="card">
-              <img src={p.image} alt={p.name} />
-              <div class="card-body">
-                <h3>{p.name}</h3>
-                <p>
-                  {p.evolutions && p.evolutions.length !== 0 && (
-                    <p>
-                      Evolutions:
-                      {p.evolutions.map((e, j) => {
-                        return <p key={j}> {e.name} </p>;
-                      })}
-                    </p>
-                  )}
-                </p>
-              </div>
+        <div className="card">
+          <img src={p.image} alt={p.name} />
+          <div className="card-body">
+            <h3>{p.name}</h3>
+            <div>
+              {p.evolutions && p.evolutions.length !== 0 && (
+                <div style={{display:"inline"}}>
+                  Evolutions:
+                  {p.evolutions.map((e, i) => {
+                    return <div style={{display:"inline"}} key={i}> {e.name} </div>;
+                  })}
+                </div>
+              )}
             </div>
-          ))}
+          </div>
+        </div>
       </div>
     </>
   );
